323aaab3773e8f6e2ecb785f7cb0c02b69e721aa
Rowney Ouma <oumarowney@gmail.com>
2025-04-21 21:06:30 +0300

    All done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const taskForm = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskDescription = taskInput.value.trim();
+    if (taskDescription !== '') {
+      const newTaskLi = document.createElement('li');
+      newTaskLi.textContent = taskDescription;
+      taskList.appendChild(newTaskLi);
+      taskInput.value = '';
+    } else {
+      console.log("Please enter a task description.");
+    }
+  });
+
+});
